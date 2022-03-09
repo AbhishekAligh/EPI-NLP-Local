eadd2155b467a93fa99ae815a00d8c3de69a9d65
AbhishekAligh <abhishekaligh@AbhishekAlighs-Mac-mini.local>
2022-03-09 19:06:32 +0530

    hide header items and annotation popups

--- src/components/WebViewer.vue
+++ src/components/WebViewer.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="webviewer" ref="viewer"></div>
+  <div>
+    <button @click="clickToAnnotate">Annotate</button>
+    <div id="webviewer" ref="viewer"></div>
+  </div>
 </template>
 
 <script>
@@ -17,6 +20,7 @@ export default {
         { page: 1, x: 80, y: 90, w: 50, h: 20, id: "2" },
         { page: 1, x: 120, y: 120, w: 50, h: 20, id: "3" },
       ],
+      webViewerInstance: {},
     };
   },
   mounted() {
@@ -27,13 +31,33 @@ export default {
       },
       this.$refs.viewer
     ).then((instance) => {
+      this.webViewerInstance = instance;
       const { docViewer, annotManager, Annotations } = instance;
-      instance.setTheme("dark");
-      instance.disableElements(["searchButton"]);
+      instance.setTheme("light");
+      instance.disableElements([
+        "searchButton",
+        "freeHandToolGroupButton",
+        "eraserToolButton",
+        "textToolGroupButton",
+        "shapeToolGroupButton",
+        "stickyToolButton",
+        "freeTextToolButton",
+        "signatureToolButton",
+        "highlightToolButton2",
+        "highlightToolButton3",
+        "highlightToolButton4",
+        "underlineToolButton",
+        "squigglyToolButton",
+        "copyTextButton",
+        "linkButton",
+        "textStrikeoutToolButton",
+        "textSquigglyToolButton",
+        "textUnderlineToolButton",
+        "strikeoutToolButton",
+      ]);
       docViewer.setMargin(40);
 
       docViewer.on("documentLoaded", () => {
-
         for (let annotId = 0; this.annotations.length; annotId++) {
           let annot = this.annotations[annotId];
           annotManager.setCurrentUser("Abhishek Aligh");
@@ -45,15 +69,32 @@ export default {
           rectangleAnnot.Height = annot.h;
           rectangleAnnot.Author = annotManager.getCurrentUser();
           rectangleAnnot.FillColor = new Annotations.Color(100, 1500, 130, 0.1);
-          rectangleAnnot.StrokeColor = new Annotations.Color(0, 0,0);
+          rectangleAnnot.StrokeColor = new Annotations.Color(0, 0, 0);
           rectangleAnnot.StrokeThickness = 5;
           rectangleAnnot.setCustomData("id", annot.id);
-
           annotManager.addAnnotation(rectangleAnnot);
           annotManager.redrawAnnotation(rectangleAnnot);
+        
         }
       });
     });
+  },
+  methods: {
+    clickToAnnotate() {
+      if (this.webViewerInstance.isElementDisabled("textToolGroupButton")) {
+        this.webViewerInstance.enableElements([
+          "textToolGroupButton",
+          "highlightToolButton",
+        ]);
+      } else {
+        this.webViewerInstance.disableElements([
+          "textToolGroupButton",
+          "highlightToolButton",
+         
+        ]);
+        console.log("Annotate button clicked");
+      }
+    },
   },
 };
 </script>
